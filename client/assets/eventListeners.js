91b9d012d91267adca6f3c09f015db76a9a85c92
azotamiota <azotamiota@gmail.com>
2022-08-10 12:08:56 +0100

    frontend files update

---
+++ client/assets/eventListeners.js
@@ -0,0 +1,6 @@
+import { createPost } from './index.js'
+
+const submitButton = document.getElementById('Publish')
+
+submitButton.addEventListener('click', createPost)
+
--- client/assets/index.js
+++ client/assets/index.js
@@ -7,7 +7,23 @@ async function createPost (e) {
             body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
         }
         const response = await fetch('http://localhost:3000/posts', options);
+        readPost();
     } catch (error) {
         console.warn(error)
     }
+}
+
+async function readPost () {
+
+    try {
+        const response = await fetch('http://localhost:3000/:title')
+        return response;
+    } catch (error) {
+        console.log('Error while fetching data')
+    }
+}
+
+module.exports = {
+    createPost,
+    readPost
 }
