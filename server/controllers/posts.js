2522760defac36c2ca4efb338f665bb9720eff21
johnpascual <ashleypascual@hotmail.es>
2022-08-10 10:35:48 +0100

    models and controllers code

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,7 +2,7 @@ const Post = require('../models/Post')
 
 async function showAll (req, res) {
     try {
-        const books = await Post.showAll();
+        const books = await Post.all;
         res.status(200).json(books)
     } catch (err) {
         res.status(500).json({err})
@@ -11,8 +11,8 @@ async function showAll (req, res) {
 
 async function create (req, res) {
     try {
-        await Book.create(req.body.title, req.body.name, req.body.story);
-        res.status(200).json({success: "new book created"})
+        await Post.create(req.body.title, req.body.name, req.body.story);
+        res.status(200).json({success: "new post created"})
     } catch (err) {
         res.status(422).json({err})
     }
--- server/models/Post.js
+++ server/models/Post.js
@@ -1,19 +1,36 @@
+const { rejects } = require("assert");
+
 class Post {
     constructor(data){
-       
         this.title = data.title;
         this.name = data.name;
         this.story = data.story;
     };
 
-    static showAll(){
-       
+    static get all(){
+        return new Promise (async (resolve, reject) => {
+            try {
+                const postData = await db.query('SELECT * FROM posts');
+                const posts = postData.rows.map(b => new Post(b));
+                resolve (posts);
+            } catch (err) {
+                reject('Posts were not found');
+            }
+        });
     };
 
     
     
-    static create(){
-        
+    static async create(postData){
+        return new Promise (async (resolve, reject) => {
+            try {
+                const newPost = await db.query('INSERT INTO posts (title, name, story) VALUES ($1, $2, $3) RETURNING*;', [ postData.title, postData.name, postData.story ])
+            const post = new Post(newPost.rows[0]);
+                resolve (post);
+            } catch (err) {
+                reject('Post could not be created')
+            }
+        })
     };
 }
 
