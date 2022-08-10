7dc19ea172b6f0a1abc62f9f34d068e89ae807c3
johnpascual <ashleypascual@hotmail.es>
2022-08-10 11:19:48 +0100

    docker fiels setup

--- server/routes/posts.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const postsController = require('../controllers/posts')
-
-
-router.get('/', postsController.showAll)
-router.post('/', postsController.create)
-
-module.exports = router;
