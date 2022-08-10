f1944bb05f0978a6a67d99380aab4e8a5886929c
johnpascual <ashleypascual@hotmail.es>
2022-08-10 11:26:46 +0100

    fixed docker bugs

--- db/migrations/1_create_post.sql
+++ db/migrations/1_create_post.sql
@@ -4,5 +4,5 @@ CREATE TABLE posts (
     id serial PRIMARY KEY,
     title varchar(200) NOT NULL,
     name varchar(100) NOT NULL,
-    story varchar(500) NOT NULL,
+    story varchar(500) NOT NULL
 );
